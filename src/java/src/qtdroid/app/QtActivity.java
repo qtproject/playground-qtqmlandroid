526d3cb643bb348fe631c212c7133c38024657fd
J-P Nurmi <jpnurmi@theqtcompany.com>
2014-12-03 19:10:03 +0100

    QtActivity: set background color/drawable

--- src/java/src/qtdroid/app/QtActivity.java
+++ src/java/src/qtdroid/app/QtActivity.java
@@ -3,6 +3,11 @@ package qtdroid.app;
 import java.lang.String;
 import java.util.ArrayList;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.R.attr;
+import android.util.TypedValue;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,6 +15,17 @@ public class QtActivity extends org.qtproject.qt5.android.bindings.QtActivity
 {
     public QtActivity() {
         m_menuItems = new ArrayList<String>();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TypedValue attr = new TypedValue();
+        getTheme().resolveAttribute(android.R.attr.windowBackground, attr, true);
+        if (attr.type >= TypedValue.TYPE_FIRST_COLOR_INT && attr.type <= TypedValue.TYPE_LAST_COLOR_INT)
+            getWindow().setBackgroundDrawable(new ColorDrawable(attr.data));
+        else
+            getWindow().setBackgroundDrawableResource(attr.resourceId);
     }
 
     @Override
